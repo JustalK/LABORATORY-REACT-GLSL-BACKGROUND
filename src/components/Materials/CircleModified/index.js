f7656475ac9197c19ee5758816501a6cc8f4ca6f
Latsuj <justal.kevin@gmail.com>
2021-08-01 00:16:35 +0800

    [FEATURE] Testing the modulation of a circle

---
+++ src/components/Materials/CircleModified/index.js
@@ -0,0 +1,53 @@
+import * as THREE from 'three'
+import { extend } from '@react-three/fiber'
+
+export default class CircleModifiedMaterial extends THREE.ShaderMaterial {
+  constructor() {
+    super({
+      uniforms: {
+        uResolution: {
+          value: new THREE.Vector2(1.0, 1.0)
+        }
+      },
+      vertexShader: `
+      varying vec2 vUv;
+      void main() {
+        vec3 pos = position;
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(pos,1.);
+      }`,
+      fragmentShader: `
+      uniform vec2 uResolution;
+      varying vec2 vUv;
+      void main()  {
+          vec2 newvUv = vUv * uResolution;
+          vec3 color = vec3(0.0);
+          vec2 pos = vec2(0.5) * uResolution -newvUv;
+
+          float r = length(pos)*2.0;
+          float a = atan(pos.y,pos.x);
+
+          float f = cos(a*2.0);
+          // float f = abs(cos(a*3.));
+          // float f = abs(cos(a*2.5))*.5+.3;
+          // float f = abs(cos(a*12.)*sin(a*3.))*.8+.1;
+          // float f = smoothstep(-.5,1., cos(a*10.))*0.2+0.5;
+
+          color = vec3( smoothstep(f,f,r) );
+
+          gl_FragColor = vec4(color, 1.0);
+      }
+      `
+    })
+  }
+
+  get uResolution() {
+    return this.uniforms.uResolution.value
+  }
+
+  set uResolution(v) {
+    return (this.uniforms.uResolution.value = v)
+  }
+}
+
+extend({ CircleModifiedMaterial })
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Experience3 from '@pages/Experience3'
 import Experience4 from '@pages/Experience4'
 import Experience5 from '@pages/Experience5'
 import Experience6 from '@pages/Experience6'
+import Experience7 from '@pages/Experience7'
 
 export default {
   Experience1,
@@ -11,5 +12,6 @@ export default {
   Experience3,
   Experience4,
   Experience5,
-  Experience6
+  Experience6,
+  Experience7
 }
