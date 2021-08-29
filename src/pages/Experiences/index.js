8bb7b540ef4bda4986445e15f621de82952b77b5
Latsuj <justal.kevin@gmail.com>
2021-08-29 16:07:41 +0200

    [FEATURE] Postprocessing

--- src/pages/Experiences/index.js
+++ src/pages/Experiences/index.js
@@ -1,7 +1,9 @@
 import Experience1 from '@pages/Experiences/Experience1'
 import Experience2 from '@pages/Experiences/Experience2'
+import Experience3 from '@pages/Experiences/Experience3'
 
 export default {
   Experience1,
-  Experience2
+  Experience2,
+  Experience3
 }
