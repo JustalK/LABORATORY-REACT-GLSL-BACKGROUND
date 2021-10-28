8f37aa5cb7de0c70aceefcffc221f43ef42b2b49
justalk <justal.kevin@gmail.com>
2021-10-28 13:42:38 +0200

    [FEATURE] Adding multiple stars with color

--- src/components/Materials/Starfields/index.js
+++ src/components/Materials/Starfields/index.js
@@ -42,7 +42,31 @@ const StarfieldsMaterial = shaderMaterial(
       rays = max(0.0, (1.0 - abs(uv.x * uv.y * 1000.0)));
       m += rays*0.3*flare;
 
+      m *= smoothstep(1.0, 0.2, d);
       return m;
+    }
+
+    vec3 StarLayer(vec2 uv) {
+      vec3 col = vec3(0);
+
+      vec2 gv = fract(uv) - 0.5;
+      vec2 id = floor(uv);
+
+      for(float y=-1.0; y <= 1.0 ;y++) {
+        for(float x=-1.0; x <= 1.0 ;x++) {
+          vec2 offs = vec2(x, y);
+
+          float n = Hash21(id + offs);
+          float size = fract(n * 12.564684);
+          float star = Star(gv - offs - vec2(n, fract(n * 34.0)) + 0.5, smoothstep(0.8, 0.9, size));
+          vec3 color = sin(vec3(0.2, 0.3, 0.9)*fract(n*1265.5255)*123.25)*0.5+0.5;
+          color *= vec3(1.0, 0.5, 1.0+size);
+
+          col += star*size*color;
+        }
+      }
+      if(gv.x > 0.48 || gv.y < -0.48) col.r = 1.0;
+      return col;
     }
 
     void mainImage( out vec4 fragColor, in vec2 fragCoord )
@@ -50,17 +74,10 @@ const StarfieldsMaterial = shaderMaterial(
       vec2 uv = fragCoord/iResolution.xy;
       uv -= 0.5;
       uv.x *= iResolution.x/iResolution.y;
-      uv *= 3.0;
+      uv *= 5.0;
 
       vec3 col = vec3(0);
-
-      vec2 gv = fract(uv) - 0.5;
-      vec2 id = floor(uv);
-
-      float n = Hash21(id);
-      col += Star(gv - vec2(n, fract(n * 34.0)) + 0.5, 1.0);
-
-      if(gv.x > 0.48 || gv.y < -0.48) col.r = 1.0;
+      col += StarLayer(uv);
 
       fragColor = vec4(col,1.0); // Output to screen
     }
