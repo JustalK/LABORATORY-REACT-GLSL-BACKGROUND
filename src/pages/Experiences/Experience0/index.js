c6e4ed1750381961f239ccbf90b229a66c8a02e5
justalk <justal.kevin@gmail.com>
2021-10-28 05:17:43 +0200

    [REFACTORING] Chaning the default experiment

--- src/pages/Experiences/Experience0/index.js
+++ src/pages/Experiences/Experience0/index.js
@@ -3,7 +3,9 @@
  * @module Experiences/Experience0
  */
 import React, { useRef } from 'react'
-import { useFrame, useThree } from '@react-three/fiber'
+import { useFrame, useThree, useLoader } from '@react-three/fiber'
+import * as THREE from 'three'
+import defaultMaterial from '@components/Materials/Default'
 
 /**
  * @function Experience
@@ -13,16 +15,26 @@ import { useFrame, useThree } from '@react-three/fiber'
 const Experience = () => {
   const { viewport } = useThree()
   const ref = useRef()
+  const [iTexture] = useLoader(THREE.TextureLoader, ['./bg.jpeg'])
+  if (iTexture) {
+    iTexture.wrapS = THREE.RepeatWrapping
+    iTexture.wrapT = THREE.RepeatWrapping
+  }
+  const { size } = useThree()
 
   useFrame((state, delta) => {
-    ref.current.uTime += delta
+    ref.current.iTime += delta
   })
 
   return (
     <>
       <mesh>
         <planeGeometry args={[viewport.width, viewport.height, 32, 32]} />
-        <meshBasicMaterial color="red" ref={ref} />
+        <voronoiMaterial
+          iResolution={[size.width, size.height, 1]}
+          ref={ref}
+          iTexture={iTexture}
+        />
       </mesh>
     </>
   )
